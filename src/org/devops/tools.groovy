5393d0a94c3f2a2d789b6d06f5164b185918a8ce
kuuun <1835747+kuuun@user.noreply.gitee.com>
2021-04-24 22:35:03 +0800

    add ansicolor

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,5 +1,12 @@
 package org.devops
 
-def printMsg(msg){
-    println(msg)
+def printMsg(msg,color){
+    colors = [
+        'red': "\033[40;31m >>>>>>>>>>>>>>>>>>>> ${msg} <<<<<<<<<<<<<<<<<<<< \033[0m",
+        'blue': "\033[40;34m >>>>>>>>>>>>>>>>>>>> ${msg} <<<<<<<<<<<<<<<<<<<< \033[0m",
+        'grenn': "\033[40;32m >>>>>>>>>>>>>>>>>>>> ${msg} <<<<<<<<<<<<<<<<<<<< \033[0m",
+    ]
+    ansiColor('xterm'){
+        println(colors[color])
+    }
 }
