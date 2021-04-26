804b76ed38d72f327cbf0450a042d9b4952985a4
kuuun <1835747+kuuun@user.noreply.gitee.com>
2021-04-26 19:11:08 +0800

    update src/org/devops/Deploy.groovy.

--- src/org/devops/Deploy.groovy
+++ src/org/devops/Deploy.groovy
@@ -4,8 +4,8 @@ def saltDeploy(hosts,command){
     sh "salt -L ${hosts} ${command}"
 }
 
-def ansibleDeploy(hosts,command,options=null){
-    if("${options}"== null){
+def ansibleDeploy(hosts,command,options=""){
+    if("${options}"== ""){
         sh "ansible ${hosts} -m ${command}"
     }else{
         sh "ansible ${hosts} -m ${command} -a ${options}"
