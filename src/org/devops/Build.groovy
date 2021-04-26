6ef0491eac1f269ac02e242c074e6b34e7e60388
kuuun <1835747+kuuun@user.noreply.gitee.com>
2021-04-26 17:14:00 +0800

    update src/org/devops/Build.groovy.

--- src/org/devops/Build.groovy
+++ src/org/devops/Build.groovy
@@ -5,6 +5,10 @@ def build(btype,bshell){
     def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NODE"]
 
     buildHome = tool buildTools[btype]
-
+    if ["${btype}" == "npm"]{
+        sh """
+            PATH=$PATH:${buildHome}/bin
+        """
+    }
     sh "${buildHome}/bin/${btype} ${bshell}"
 }
