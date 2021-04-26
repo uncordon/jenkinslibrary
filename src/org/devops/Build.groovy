2725b46ab87cb79f382449798c12cf0a36a27c9e
kuuun <1835747+kuuun@user.noreply.gitee.com>
2021-04-26 16:54:34 +0800

    update src/org/devops/Build.groovy.

--- src/org/devops/Build.groovy
+++ src/org/devops/Build.groovy
@@ -2,7 +2,7 @@ package org.devops
 
 //构建
 def build(btype,bshell){
-    def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NODEJS"]
+    def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NODE"]
 
     buildHome = tool buildTools[btype]
 
