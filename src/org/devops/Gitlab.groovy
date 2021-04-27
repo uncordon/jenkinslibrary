128c7920d68495f814ea8a11d1c202984e31335a
kuuun <1835747+kuuun@user.noreply.gitee.com>
2021-04-27 18:59:13 +0800

    update src/org/devops/Gitlab.groovy.

--- src/org/devops/Gitlab.groovy
+++ src/org/devops/Gitlab.groovy
@@ -2,19 +2,22 @@ package org.devops
 
 def gitlabHttpAPI(httpHost,crtId,action,url){
     withCredentials([string(credentialsId: crtId, variable: 'ACCESS_TOKEN')]) {
-        httpRequest contentType: 'APPLICATION_JSON', 
-            customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: '${ACCESS_TOKEN}']], 
-            httpMode: 'POST', 
-            ignoreSslErrors: true, 
-            responseHandle: 'NONE', 
-            url: '${httpHost}/api/v4/${url}', 
-            wrapAsMultipart: false
+        res = httpRequest contentType: 'APPLICATION_JSON', 
+                customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: '${ACCESS_TOKEN}']], 
+                httpMode: 'POST', 
+                ignoreSslErrors: true, 
+                responseHandle: 'NONE', 
+                url: '${httpHost}/api/v4/${url}', 
+                wrapAsMultipart: false
     }
+    return res
 }
 
 def gitlabBuildStatus(httpHost,crtId,projectId,commitId,status){
     String url = "projects/${projectId}/statuses/${commitId}?state=${status}"
-    gitlabHttpAPI(httpHost,crtId,'POST',url)
+    response = gitlabHttpAPI(httpHost,crtId,'POST',url)
+    println(response)
+    return response
 }
 
 def getHttpHost(httpUrl){
