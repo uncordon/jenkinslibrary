22b59a7576a39191951a7419f5e9818b9712b842
kuuun <vipzhanghaokun@163.com>
2021-04-28 12:30:52 +0800

    update src/org/devops/Email.groovy.

--- src/org/devops/Email.groovy
+++ src/org/devops/Email.groovy
@@ -8,7 +8,7 @@ def sendEmail(status,toUser){
             <meta charset="UTF-8"> 
             </head> 
             <body leftmargin="8" marginwidth="0" topmargin="8" marginheight="4" offset="0"> 
-                <img src="${JENKINS_URL}static/aaaf8f1b/images/jenkins-header-logo-v2.svg">
+                <img src="${JENKINS_URL}/static/aaaf8f1b/images/jenkins-header-logo-v2.svg">
                 <table width="95%" cellpadding="0" cellspacing="0" style="font-size: 11pt; font-family: Tahoma, Arial, Helvetica, sans-serif">   
                     <tr> 
                         <td><br /> 
