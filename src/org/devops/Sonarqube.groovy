b48556b44afa0d9bf312b0f8ce24bf11e0c8a571
kuuun <vipzhanghaokun@163.com>
2021-04-28 19:06:34 +0800

    update src/org/devops/Sonarqube.groovy.

--- src/org/devops/Sonarqube.groovy
+++ src/org/devops/Sonarqube.groovy
@@ -1,23 +1,28 @@
 package org.devops
 
-def sonarScan(projectName,sonarServe,username,password,projectDescription,projectPath){
+class Sonarqube(){
+    String sonarServe
+    String username
+    String password
     String sonarDate = sh returnStdout:true,script:"date +%s"
     sonarDate = sonarDate - "\n"
 
-    sh """
-    sonar-scanner  -Dsonar.host.url=${sonarServe}  \
-        -Dsonar.projectKey=${projectName}  \
-        -Dsonar.projectName=${projectName} \
-        -Dsonar.projectVersion=${sonarDate} \
-        -Dsonar.login=${username} \
-        -Dsonar.password=${password} \
-        -Dsonar.ws.timeout=30 \
-        -Dsonar.projectDescription=${projectDescription}  \
-        -Dsonar.links.homepage=http://www.baidu.com \
-        -Dsonar.sources={projectPath} \
-        -Dsonar.sourceEncoding=UTF-8 \
-        -Dsonar.java.binaries=target/classes \
-        -Dsonar.java.test.binaries=target/test-classes \
-        -Dsonar.java.surefire.report=target/surefire-reports
-    """
+    def sonarJava(projectName,projectDescription,projectPath){
+        sh """
+        sonar-scanner  -Dsonar.host.url=${sonarServe}  \
+            -Dsonar.projectKey=${projectName}  \
+            -Dsonar.projectName=${projectName} \
+            -Dsonar.projectVersion=${sonarDate} \
+            -Dsonar.login=${username} \
+            -Dsonar.password=${password} \
+            -Dsonar.ws.timeout=30 \
+            -Dsonar.projectDescription=${projectDescription}  \
+            -Dsonar.links.homepage=http://www.baidu.com \
+            -Dsonar.sources={projectPath} \
+            -Dsonar.sourceEncoding=UTF-8 \
+            -Dsonar.java.binaries=target/classes \
+            -Dsonar.java.test.binaries=target/test-classes \
+            -Dsonar.java.surefire.report=target/surefire-reports
+        """
+    }
 }
