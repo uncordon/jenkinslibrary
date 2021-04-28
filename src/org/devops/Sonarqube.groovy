f56fd403f40cbd8d60c78047283c67a8af9fdcc7
kuuun <vipzhanghaokun@163.com>
2021-04-28 22:27:16 +0800

    update src/org/devops/Sonarqube.groovy.

--- src/org/devops/Sonarqube.groovy
+++ src/org/devops/Sonarqube.groovy
@@ -1,14 +1,19 @@
 package org.devops
 
 def sonarJava(sonarServe,username,password,projectName,projectDescription,projectPath){
+    withSonarQubeEnv('SONAR', envOnly: true) {
+        // This expands the evironment variables SONAR_CONFIG_NAME, SONAR_HOST_URL, SONAR_AUTH_TOKEN that can be used by any script.
+        println ${env.SONAR_CONFIG_NAME}
+        println ${env.SONAR_HOST_URL}
+        println ${env.SONAR_AUTH_TOKEN}
     String SONAR_SCAN_CLI_HOME = "/usr/local/sonar-scanner-cli"
     String sonarDate = sh returnStdout:true,script:"date +%Y%m%d%H%M%S"
     sonarDate = sonarDate - '\n'
     
     sh """
         ${SONAR_SCAN_CLI_HOME}/bin/sonar-scanner  -Dsonar.host.url=${sonarServe}  \
-        -Dsonar.login=${username} \
-        -Dsonar.password=${password} \
+        #-Dsonar.login=${username} \
+        #-Dsonar.password=${password} \
         -Dsonar.ws.timeout=30 \
         -Dsonar.projectKey=${projectName}  \
         -Dsonar.projectName=${projectName} \
@@ -21,4 +26,5 @@ def sonarJava(sonarServe,username,password,projectName,projectDescription,projec
         -Dsonar.java.test.binaries=target/test-classes \
         -Dsonar.java.surefire.report=target/surefire-reports
     """
+    }
 }
