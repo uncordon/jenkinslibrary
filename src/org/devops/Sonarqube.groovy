ec23471fa3b2e42a90762344a19f31cd9d33a61d
kuuun <vipzhanghaokun@163.com>
2021-04-28 19:37:59 +0800

    update src/org/devops/Sonarqube.groovy.

--- src/org/devops/Sonarqube.groovy
+++ src/org/devops/Sonarqube.groovy
@@ -1,11 +1,12 @@
 package org.devops
 
 def sonarJava(sonarServe,username,password,projectName,projectDescription,projectPath){
+    String SONAR_SCAN_CLI_HOME = "/usr/local/sonar-scanner-cli"
     String sonarDate = sh returnStdout:true,script:"date +%Y%m%d%H%M%S"
     sonarDate = sonarDate - '\n'
     
     sh """
-    /usr/local/sonar-scanner-cli/bin/sonar-scanner  -Dsonar.host.url=${sonarServe}  \
+        ${SONAR_SCAN_CLI_HOME}/bin/sonar-scanner  -Dsonar.host.url=${sonarServe}  \
         -Dsonar.login=${username} \
         -Dsonar.password=${password} \
         -Dsonar.ws.timeout=30 \
