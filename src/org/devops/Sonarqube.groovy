054e5640453a2f52b2776f925012cb338b27b957
kuuun <vipzhanghaokun@163.com>
2021-04-29 15:35:08 +0800

    update src/org/devops/Sonarqube.groovy.

--- src/org/devops/Sonarqube.groovy
+++ src/org/devops/Sonarqube.groovy
@@ -42,26 +42,3 @@ def sonarJava(sonarServer,projectName,projectDescription,projectPath){
     */
     
 }
-
-// 请求HTTP的URL进行拼接
-def getSonarStatus(httpHost,projectName,crtId){
-    String url  = "project_branches/list?project=${projectName}"
-    String apiUrl = "${httpHost}/api/${url}"
-
-    response = sonarHttpRequest(apiUrl,crtId)
-    responseJSON = readJSON text: """${response.content}"""
-    
-    return responseJSON.branches[0].status.qualityGateStatus
-}
-
-// 发起GET请求
-def sonarHttpRequest(apiUrl,crtId){
-    res = httpRequest authentication: crtId, 
-        contentType: 'APPLICATION_JSON', 
-        ignoreSslErrors: true, 
-        responseHandle: 'NONE', 
-        url: apiUrl, 
-        wrapAsMultipart: false
-
-    return res
-}
